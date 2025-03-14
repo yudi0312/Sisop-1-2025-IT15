69126c7f1b3fcb42ff1a2c4fe375b75b8cf343fb
yudi0312 <putuyudi1010@gmail.com>
2025-03-15 01:30:47 +0700

    pushsoal1

--- soal_1/poppo_siroyo.sh
+++ soal_1/poppo_siroyo.sh
@@ -1,20 +1,26 @@
 #!/bin/bash 
 wget -q "https://drive.usercontent.google.com/u/0/uc?id=1l8fsj5LZLwXBlHaqhfJVjz_T0p7EJjqV&export=download" -O reading_data.csv
 FILE="reading_data.csv"
-
+clear_screen(){
+    clear
+}
 if [ "$1" = "soalA" ]; then
+    clear_screen
     count=$(awk -F, 'tolower($2) == "chris hemsworth" {count++} END {print count}' "$FILE")
     echo "Chris Hemsworth membaca $count buku."
 
 elif [ "$1" = "soalB" ]; then
+    clear_screen
     Reading_Duration_Minutes=$(awk -F, '$8 == "Tablet" {sum+=$6; count++} END {if (count>0) print sum/count; else print 0}' "$FILE")
     echo "Rata-rata durasi membaca dengan Tablet adalah $Reading_Duration_Minutes menit."
     
 elif [ "$1" = "soalC" ]; then
+    clear_screen
     highest_rating=$(awk -F, 'NR>1 {if ($7 > max) {max=$7; name=$2; book=$3}} END {print name " - " book " - " max}' "$FILE")
     echo "Pembaca dengan rating tertinggi: $highest_rating"
 
 elif [ "$1" = "soalD" ]; then 
+    clear_screen
     result=$(awk -F, '
     $9 == "Asia" && $5 > "2023-12-31" {
         count[$4]++} END {
@@ -27,3 +33,8 @@ elif [ "$1" = "soalD" ]; then
         printf "Genre paling populer di Asia setelah 2023 adalah %s dengan %d buku.\n", genre, max
     }' "$FILE")
     echo "$result"
+else
+    echo -e "\033[1;31mError: command '$1' tidak sesuai\033[0m"
+    echo -e "\033[1;32mSilahkan pilih command [soalA/soalB/soalC/soalD]\033[0m"
+    exit 1
+fi 
